1f47e90b98ae98619df610ecffa19b1b882d102a
FernandoALopes92 <fernando.lopes_92@hotmail.com>
2022-05-22 10:53:34 -0300

    Exercicio Metodo Construtor

---
+++ show/src/Artista.java
@@ -0,0 +1,11 @@
+public class Artista {
+    String nome;
+    float cache;
+
+    public Artista(){}
+
+    public Artista(String nome,float cache){
+        this.nome = nome;
+        this.cache = cache;
+    }
+}
