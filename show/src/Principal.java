7bba5a9a5ad934081d7efb965ab56b802a3086e6
FernandoALopes92 <fernando.lopes_92@hotmail.com>
2022-05-22 12:21:34 -0300

    Metodos acessores

--- show/src/Principal.java
+++ show/src/Principal.java
@@ -3,8 +3,5 @@ public class Principal {
     public static void main(String[] args) {
         Show show =  new Show(new Artista("Michael Jackson", 1000000));
         System.out.println(show.artista.nome);
-
-
-
     }
 }
