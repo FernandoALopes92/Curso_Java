335a8a4af09b7825c75565f58099f94ee1d14d3d
FernandoALopes92 <fernando.lopes_92@hotmail.com>
2022-06-26 11:31:07 -0300

    polimorfismo de sobreposição

---
+++ sobreposicao/src/ClienteComum.java
@@ -0,0 +1,7 @@
+public class ClienteComum extends Cliente{
+
+    @Override
+    public void entrarEmContato(){
+        System.out.println("Entrando em contato via central");
+    }
+}
---
+++ sobreposicao/src/ClienteFidelidade.java
@@ -0,0 +1,8 @@
+public class ClienteFidelidade extends Cliente {
+
+
+    @Override
+    public void entrarEmContato() {
+        System.out.println("Entrando em contato com gerente");
+    }
+}
